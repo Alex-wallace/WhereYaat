fe7cfde2ab571e09ac0e2c54f55de24ca8553e45
Alex-wallace <alex_wallace_25@outlook.com>
2017-04-22 02:28:03 -0400

    Added locTable classes Added items to the spinner dynamically

--- app/src/main/java/ca/wallacemohawcollege/alex/whereya/Mapping.java
+++ app/src/main/java/ca/wallacemohawcollege/alex/whereya/Mapping.java
@@ -1,7 +1,13 @@
 package ca.wallacemohawcollege.alex.whereya;
 
+import android.database.DataSetObserver;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,18 +16,34 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mapping extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    List<String> names = new ArrayList<String>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapping);
+        names.add(0,"first");
+        names.add(1,"second");
+        names.add(2,"third");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,names) ;
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner favs = (Spinner) findViewById(R.id.spinner2);
+        favs.setAdapter(adapter);
+
+
     }
 
 
